93be4e9db68ddb614c7b87f55ad6eb4150dc5a6d
domai <1611997621@qq.com>
2024-02-26 02:55:19 +0800

    1

---
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -0,0 +1,32 @@
+package cn.itcast.order;
+
+
+import cn.itcast.feign.clients.UserClient;
+import cn.itcast.feign.config.DefaultFeignConfiguration;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@MapperScan("cn.itcast.order.mapper")
+@SpringBootApplication
+@EnableFeignClients(clients = {UserClient.class} ,defaultConfiguration = DefaultFeignConfiguration.class)
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+    /**
+     * 创建 RestTemplate 实例，并放入 spring 容器
+     * @return RestTemplate
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
